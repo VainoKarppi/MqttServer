1fb5c540bc488cd62540e4ce45d4ff1043cfeb1a
Väinö Karppi <vaino.karppi@gmail.com>
2024-01-31 13:41:48 +0200

    Added server to check for client messages

--- MqttServer/Program.cs
+++ MqttServer/Program.cs
@@ -1,5 +1,4 @@
-﻿using MQTTnet.Samples.Server;
-using MQTTnet.Diagnostics;
+﻿using MQTTnet.Diagnostics;
 using MQTTnet.Protocol;
 using MQTTnet.Server;
 using MQTTnet;
@@ -11,19 +10,19 @@ using System.Text;
 
 
 //! START SERVER
-var mqttFactory = new MqttFactory(new ConsoleLogger());
+var mqttFactory = new MqttFactory();
+//var mqttFactory = new MqttFactory(new ConsoleLogger());
 var options = new MqttServerOptionsBuilder().WithDefaultEndpoint().WithDefaultEndpointPort(1234).Build();
 
 using MqttServer Server = mqttFactory.CreateMqttServer(options);
+
+// Subscribe to all client messages
+Server.ApplicationMessageNotConsumedAsync += ClientMessageEvent;
 
 
 await Server.StartAsync();
 Console.WriteLine("Server Started!");
 
-
-_ = new Thread(() => {
-    // READ DATA AND STORE TO JSON
-});
 
 Console.WriteLine("\nCommands: Exit, Send\n");
 while (true)
@@ -32,8 +31,7 @@ while (true)
     if (string.IsNullOrEmpty(input)) continue;
     if (input == "exit") break;
 
-    if (input == "send")
-    {
+    if (input == "send") {
         Console.WriteLine("\nEnter topic: ");
         string? topic = Console.ReadLine();
         Console.WriteLine("\nEnter payload: ");
@@ -42,6 +40,20 @@ while (true)
     }
 
 }
+
+
+
+static Task ClientMessageEvent(ApplicationMessageNotConsumedEventArgs args) {
+    string topic = args.ApplicationMessage.Topic;
+    string message = Encoding.UTF8.GetString(args.ApplicationMessage.PayloadSegment);
+
+    Console.WriteLine(topic);
+    Console.WriteLine(message);
+
+    return Task.CompletedTask;
+}
+
+
 
 
 async Task SendData(string? topic, string? mode) {
@@ -53,33 +65,14 @@ async Task SendData(string? topic, string? mode) {
     Console.WriteLine(message.ConvertPayloadToString());
 
     // Now inject the new message at the broker.
-    await Server.InjectApplicationMessage(
-        new InjectedMqttApplicationMessage(message)
-        {
-            SenderClientId = "SenderClientId"
-        });
+    await Server.InjectApplicationMessage(new InjectedMqttApplicationMessage(message) {
+        SenderClientId = "SenderClientId"
+    });
 }
 
 
 
 
-
-
-
-
-
-
-/*
-private static Task HandleReceivedMessage(MqttApplicationMessageReceivedEventArgs eventArgs) {
-    // Handle the received message
-    string topic = eventArgs.ApplicationMessage.Topic;
-    string payload = Encoding.UTF8.GetString(eventArgs.ApplicationMessage.Payload);
-    Console.WriteLine($"Received message on topic '{topic}': {payload}");
-
-    // You can add your custom message handling logic here
-
-    return Task.CompletedTask;
-}*/
 
 
 
@@ -130,58 +123,3 @@ class ConsoleLogger : IMqttNetLogger {
 }
 
 
-
-
-sealed class MqttRetainedMessageModel {
-    public string? ContentType { get; set; }
-    public byte[]? CorrelationData { get; set; }
-    public byte[]? Payload { get; set; }
-    public MqttPayloadFormatIndicator PayloadFormatIndicator { get; set; }
-    public MqttQualityOfServiceLevel QualityOfServiceLevel { get; set; }
-    public string? ResponseTopic { get; set; }
-    public string? Topic { get; set; }
-    public List<MqttUserProperty>? UserProperties { get; set; }
-
-    public static MqttRetainedMessageModel Create(MqttApplicationMessage message)
-    {
-        if (message == null)
-        {
-            throw new ArgumentNullException(nameof(message));
-        }
-
-        return new MqttRetainedMessageModel
-        {
-            Topic = message.Topic,
-
-            // Create a copy of the buffer from the payload segment because 
-            // it cannot be serialized and deserialized with the JSON serializer.
-            Payload = message.PayloadSegment.ToArray(),
-            UserProperties = message.UserProperties,
-            ResponseTopic = message.ResponseTopic,
-            CorrelationData = message.CorrelationData,
-            ContentType = message.ContentType,
-            PayloadFormatIndicator = message.PayloadFormatIndicator,
-            QualityOfServiceLevel = message.QualityOfServiceLevel
-
-            // Other properties like "Retain" are not if interest in the storage.
-            // That's why a custom model makes sense.
-        };
-    }
-
-    public MqttApplicationMessage ToApplicationMessage()
-    {
-        return new MqttApplicationMessage
-        {
-            Topic = Topic,
-            PayloadSegment = new ArraySegment<byte>(Payload ?? Array.Empty<byte>()),
-            PayloadFormatIndicator = PayloadFormatIndicator,
-            ResponseTopic = ResponseTopic,
-            CorrelationData = CorrelationData,
-            ContentType = ContentType,
-            UserProperties = UserProperties,
-            QualityOfServiceLevel = QualityOfServiceLevel,
-            Dup = false,
-            Retain = true
-        };
-    }
-}
