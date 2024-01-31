b9ecd56b15861566b83a4fdf018c43f0b67f9529
Jouni Juvonen <ejoju004@edu.xamk.fi>
2024-01-31 14:00:21 +0200

    added temperature avg calculation

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -12,16 +12,42 @@ const char *ssid = "MB210-G";
 const char *password = "studentMAMK";
 
 const char *mqtt_server = "172.20.50.151";
-const int mqtt_port = 1234;
-const char* mqtt_username = "esp32-test-user";
-
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 const int LED_OUTPUT_PIN = 19; // debug led
 
-DHT dht(DHTPIN, DHTTYPE); // DHT object
+DHT dht(DHTPIN, DHTTYPE);
+
+/* Variables for tracking the measurements for average calculation */
+int amountOfMeasurements = 5;
+int measurementCounter = 0;
+
+/* Measurements array */
+float measurements[] = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+/* Function that is used to add or update new measurement in the list */
+void addMeasurement(float temperature) {
+	measurements[measurementCounter] = temperature;
+	/* Update position */
+	measurementCounter += 1;
+  	/* Start again from the first item is the array is full */
+	if (measurementCounter == amountOfMeasurements) {
+		measurementCounter = 0;
+	}
+}
+
+/* Function to calculate the average of measured temperatures */
+float calculateAverage() {
+	float avgTemp = 0.0;
+	for (int counter = 0 ; counter < amountOfMeasurements ; counter++) {
+		avgTemp += measurements[counter];
+	}
+	/* Calculate the average temp */
+	avgTemp = avgTemp / amountOfMeasurements;
+	return(avgTemp);
+}
 
 void setup()
 {
@@ -33,29 +59,54 @@ void setup()
     if (ENABLEMQQT)
     {
         setup_wifi();
-        client.setServer(mqtt_server, mqtt_port);
+        client.setServer(mqtt_server, 1234);
         client.setCallback(callback);
     }
     dht.begin();
 }
 
-void loop() {
-    if (ENABLEMQQT) {
-        if (!client.connected()) {
+void loop()
+{
+    if (ENABLEMQQT)
+    {
+        if (!client.connected())
+        {
             reconnect();
         }
         client.loop();
     }
 
+    // Variable for measured temperature
+    float measuredTemp = 0.0;
 
+    // Read temperature from sensor
     float h = dht.readHumidity();
-    float t = dht.readTemperature();
+    measuredTemp = dht.readTemperature();
 
     Serial.print(F("Humidity: "));
     Serial.print(h);
     Serial.print(F("%  Temperature: "));
-    Serial.print(t);
+    Serial.print(measuredTemp);
     Serial.println();
+
+    // Variable for average temperature
+    float averageTemp = 0.0;
+
+    // Add measurement to list, calculate average temp and send it to mqtt
+    addMeasurement(measuredTemp);
+    averageTemp = calculateAverage();
+
+    // Print measured temperature to serial console
+    Serial.print("Average (array used): ");
+    Serial.println(averageTemp);
+
+     // Convert the average temperature value to a char array and publish it to MQTT
+    char tempString[8];
+    dtostrf(averageTemp, 1, 2, tempString);
+    mqttClient.publish("esp32/temperature", tempString);
+
+    // Sleep X seconds before next measurement
+    sleep(2);
 
     if (ENABLEMQQT)
     {
@@ -135,11 +186,14 @@ void reconnect()
 
         Serial.print("Attempting MQTT connection...");
         // Attempt to connect
-        if (client.connect("ESP32Client",mqtt_username)) {
+        if (client.connect("ESP8266Client"))
+        {
             Serial.println("connected");
             // Subscribe
             client.subscribe("hello");
-        } else {
+        }
+        else
+        {
             Serial.print("failed, rc=");
             Serial.print(client.state());
             Serial.println(" try again in 5 seconds");
