5905c7808f648408b1b5ccb326bd4f3ffd312a73
Väinö Karppi <vaino.karppi@gmail.com>
2024-02-01 15:10:42 +0200

    ADDED: IsConnectedToDatabase method

--- MqttServer/Database.cs
+++ MqttServer/Database.cs
@@ -3,11 +3,13 @@ using System;
 using MySqlConnector;
 
 static class Database {
-    public static string IP = "karppi.dy.fi";
-    public static string Username = "test";
-    public static string Password = "test";
-    public static string DatabaseName = "test";
-    public static MySqlConnection? Connection;
+
+    //TODO Add these values to environment config
+    private static readonly string IP = "karppi.dy.fi";
+    private static readonly string Username = "test";
+    private static readonly string Password = "test";
+    private static readonly string DatabaseName = "test";
+    private static MySqlConnection? Connection;
 
 
     public static Task ConnectToDatabase() {
@@ -23,6 +25,20 @@ static class Database {
         Connection.ChangeDatabase(DatabaseName);
         
         return Task.CompletedTask;
+    }
+
+    public static bool IsConnectedToDatabase() {
+        if (Connection is null) return false;
+        if (Connection.State != System.Data.ConnectionState.Open) return false;
+
+        // Make sure database can be accessed using a simple query
+        try {
+            using MySqlCommand command = new("SELECT 1", Connection);
+            command.ExecuteScalar();
+            return true; 
+        } catch (Exception) {
+            return false;
+        }
     }
 
 }
